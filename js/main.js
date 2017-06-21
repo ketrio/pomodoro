fb517a9e293b854a2e997188712efb65172a9a9b
Vlad Gutkovsky <vladgutkovskii@gmail.com>
2017-06-21 16:08:16 +0300

    Added working timer class.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,50 @@
 const clock = document.querySelector(".pomodoro");
+const input = document.querySelector("input");
 const wrap = (str) => (str.length == 2 ? str : '0' + str);
-let thread;
+let threadID;
 
-
-document.querySelector(".trigg").addEventListener('click', function (e) {
-    let future = new Date();
-    future.setTime(future.getTime() + 60000);
-    new Node
-
-    thread = window.setInterval(function(){ clock.innerHTML = timer(future, window.alert) }, 1000);
-})
-
-function timer(future, dzyn) {
-    let diff = new Date(future - new Date());
-
-    if (dzyn && diff.getTime() <= 0) {
-        dzyn("Time expired!");
-        clearInterval(thread);
-        tick.sayIt();
-        return "00:00";
+class Timer {
+    constructor(time, event) {
+        this.time = time;
+        this.event = event;
     }
 
-    let minutes = diff.getMinutes();
-    let seconds = diff.getSeconds();
+    start(action) {
+        this.now = new Date();
+        this.endDate = new Date(this.now.getTime() + this.time);
 
-    return [minutes, seconds].map(e => wrap(e.toString())).join(':');
+
+        threadID = setInterval(() => {
+            this.event();
+            this.now = new Date();
+            if (this.now > this.endDate) {
+                action();
+                clearInterval(threadID);
+            }
+        }, 1000);
+    }
+
+    getCurrentState() {
+        let state = new Date(this.endDate - this.now);
+
+        let minutes = state.getMinutes();
+        let seconds = state.getSeconds();
+
+        return [minutes, seconds].map(e => (e.length == 2 ? e : '0' + e)).join(':');
+    }
 }
+
+document.querySelector('.btn-danger').addEventListener('click', () => {
+    if (parseInt(input.value) > 1) input.value = parseInt(input.value) - 1;
+});
+
+document.querySelector('.btn-success').addEventListener('click', () => {
+    input.value = parseInt(input.value) + 1;
+});
+
+document.querySelector(".trigg").addEventListener('click', function () {
+    clearInterval(threadID);
+    let timer = new Timer(parseInt(input.value) * 60000,
+        () => clock.innerHTML = timer.getCurrentState());
+    timer.start(() => window.alert("Done!"));
+});
