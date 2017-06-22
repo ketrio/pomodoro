92bbd4a327fde3379e6d1a2efbe3273d4bd7f0c0
Vlad Gutkovsky <vladgutkovskii@gmail.com>
2017-06-22 21:33:40 +0300

    Working Pomodoro!!!

--- js/main.js
+++ js/main.js
@@ -5,21 +5,24 @@ class Timer {
         this.threadID = null;
     }
 
-    start(time, action) {
+    start(time) {
         if(this.state === states.inactive) this.time = time;
         this.now = new Date();
         this.endDate = new Date(this.now.getTime() + this.time);
         this.state = states.active;
 
-        this.threadID = setInterval(() => {
-            this.tick();
-            this.now = new Date();
-            if (this.now > this.endDate) {
-                action();
-                clearInterval(this.threadID);
-                this.state = states.inactive;
-            }
-        }, 1000);
+        return new Promise(resolve => {
+            this.threadID = setInterval(() => {
+                this.tick();
+                this.now = new Date();
+                if (this.now > this.endDate) {
+                    // action();
+                    clearInterval(this.threadID);
+                    this.state = states.inactive;
+                    resolve();
+                }
+            }, 1000);
+        })
     }
 
     getCurrentState() {
@@ -50,13 +53,61 @@ class Timer {
     }
 }
 
+class Pomodoro extends Timer {
+    constructor(tick) {
+        super(tick);
+        this.timeFor = undefined;
+    }
+
+    start(workTime, breakTime) {
+        this.workTime = workTime;
+        this.breakTime = breakTime;
+        this.startWork();
+    }
+
+    async startWork() {
+        this.timeFor = timeFor.work;
+        await super.start(this.workTime);
+        this.startBreak()
+    }
+
+    async startBreak() {
+        this.timeFor = timeFor.break;
+        await super.start(this.breakTime);
+        this.startWork();
+    }
+
+    getState() {
+        const timerState = super.getState();
+        if(timerState === states.inactive) {
+            return timeFor.inactive;
+        }
+        else {
+            return this.state;
+        }
+    }
+
+    getTimeFor() {
+        return this.timeFor;
+    }
+
+    resume() {
+        power.start();
+    }
+}
+
 const states = {
     inactive : 0,
     active : 1,
     pause : 2
 };
+const timeFor = {
+    work: 0,
+    break: 1
+};
 const clock = document.querySelector(".pomodoro");
-timer = new Timer(() => clock.innerText = timer.getCurrentState());
+let timer = new Pomodoro(() => clock.innerText = timer.getCurrentState() + '\n' +
+    (timer.getTimeFor() ? 'break' : 'work'));
 
 document.querySelectorAll('.btn-danger').forEach(e => {
     e.addEventListener('click', function() {
@@ -70,16 +121,22 @@ document.querySelectorAll('.btn-success').forEach(e => {
     e.addEventListener('click', function() {
         let groupID = this.parentElement.parentElement.id;
         let input = document.querySelector('#' + groupID + ' input');
-        input.value++;
+        if(input.value < 100) input.value++;
     });
 });
 
-document.querySelector(".trigg").addEventListener('click', function () {
+document.querySelector(".trigg").addEventListener('click', async function () {
     if(timer.getState() === states.active) {
         timer.pause();
     }
     else {
-        document.querySelectorAll('button, input').forEach(e => {if(e !== this) e.disabled = true});
-        timer.start(parseInt(document.querySelector('input').value) * 60000, () => alert('done'));
+        if (timer.getState() === states.pause) {
+            timer.resume();
+        }
+        else {
+            document.querySelectorAll('button, input').forEach(e => {if(e !== this) e.disabled = true});
+            timer.start(parseInt(document.querySelector('#work input').value) * 60000,
+                        parseInt(document.querySelector('#break input').value) * 60000);
+        }
     }
 });
